2fd6664c75006c9f9827185f92a9f76e512d7986
Andy Martinez <am41119@hotmail.com>
2016-08-15 17:55:20 +0200

    Attempt to fix ls method

--- ProtocolTesterLib.py
+++ ProtocolTesterLib.py
@@ -64,18 +64,17 @@ class ProtocolTesterLib:
 
         self._execute_command(self.command)
 
-        file_names = ["4"]
 
         if self.client == "srmls":
+            file_names_list = []
             file_names_dir = self.output.split("\n")
             print "File names dir: ", file_names_dir
             for size_name in file_names_dir:
                 file_name = size_name.split()
                 #print file_name[1]
-                file_names.append(file_name[1])
-
-        print file_names
-        return file_names
+                file_names_list.append(file_name[1])
+            print file_names_list
+            return file_names_list
 
     def copy_local_file(self, local_file, remote_file):
 
