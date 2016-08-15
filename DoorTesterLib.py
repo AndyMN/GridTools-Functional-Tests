a3113d9ffd34c859ed88e31c617294bba6ff88ed
Andy Martinez <am41119@hotmail.com>
2016-08-15 15:17:51 +0200

    Removed shared socket object

--- DoorTesterLib.py
+++ DoorTesterLib.py
@@ -4,18 +4,20 @@ import socket
 class DoorTesterLib:
 
     def __init__(self, host):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.settimeout(1)
         self.host = host
 
     def test_protocol_door(self, protocol, port):
         print "Testing port, not protocol ! Protocol is just an added string !"
         try:
-            self.socket.connect((self.host, int(port)))
+            test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            test_socket.settimeout(1)
+            test_socket.connect((self.host, int(port)))
             print "Protocol: ", protocol
             print "Port: ", str(port)
+            test_socket.close()
         except:
             print "Protocol: ", protocol
             print "Port: ", str(port)
             print "Not open !"
             raise RuntimeError("Socket couldn't connect to Port: " + str(port))
+
