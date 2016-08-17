869117b6f854339c93c03ad90b04a61caf5db3d3
Jenkins slave <jenkins@vm-dcache-wn-sl7.desy.de>
2016-08-17 17:49:40 +0200

    Changed host from prometheus to reference-2-13

--- UserDefinedVariables.py
+++ UserDefinedVariables.py
@@ -1,13 +1,15 @@
 
 
-PROTOCOL_PORTS = {"srm": "8443", "gsidcap": "22128", "gsiftp": "2811", "http": "2288", "dcap": "22125"}
+PROTOCOL_PORTS = {"srm": "8443", "gsidcap": "22128", "gsiftp": "2811", "http": "2880", "dcap": "22125"}
 
 
-HOST = "prometheus.desy.de"
+#HOST = "prometheus.desy.de"
+HOST = "reference-2-13.desy.de"
 
 
 LOCAL_DIR = "/scratch/jenkins/jenkins/workspace/robot-g2-tests/"
 LOCAL_FILE = LOCAL_DIR + "testfile"
 
-REMOTE_DIR = "/Users/kermit/"
+#REMOTE_DIR = "/Users/kermit/"
+REMOTE_DIR = "/data/g2/"
 REMOTE_FILE = REMOTE_DIR + "testo"
