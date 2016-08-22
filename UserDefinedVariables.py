2b8a97c45a4ea6a2a7cf409461ed293730d623dd
Jenkins slave <jenkins@vm-dcache-wn-sl7.desy.de>
2016-08-22 16:39:47 +0200

    Added parametrization of protocol ports

--- UserDefinedVariables.py
+++ UserDefinedVariables.py
@@ -1,15 +1,16 @@
+import os
 
-
-PROTOCOL_PORTS = {"srm": "8443", "gsidcap": "22128", "gsiftp": "2811", "http": "2880", "dcap": "22125"}
+PROTOCOL_PORTS = {"srm": os.environ.get('SRM_PORT'), "gsidcap": os.environ.get('GSIDCAP_PORT'), "gsiftp": os.environ.get('GSIFTP_PORT'), "http": os.environ.get('HTTP_PORT'), "dcap": os.environ.get('DCAP_PORT')}
 
 
 #HOST = "prometheus.desy.de"
-HOST = "reference-2-13.desy.de"
-
+#HOST = "reference-2-13.desy.de"
+HOST = os.environ.get('DFTS_SUT')
 
 LOCAL_DIR = "/scratch/jenkins/jenkins/workspace/robot-g2-tests/"
 LOCAL_FILE = LOCAL_DIR + "testfile"
 
 #REMOTE_DIR = "/Users/kermit/"
-REMOTE_DIR = "/data/g2/"
+#REMOTE_DIR = "/data/g2/"
+REMOTE_DIR = os.environ.get('REMOTE_DIR')
 REMOTE_FILE = REMOTE_DIR + "testo"
